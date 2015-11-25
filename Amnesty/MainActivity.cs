883c96052d8a85ddac53f11cb65b5de70a831360
Guades <ianemsens@gmail.com>
2015-11-25 09:57:59 +0100

    initial commit

---
+++ Amnesty/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Amnesty
+{
+	[Activity (Label = "Amnesty", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
+
